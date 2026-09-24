21fe38c8c3bb9bd4f78ad4709f0cbdb764085601
Kian Davey <kiandaveyantolin@gmail.com>
2026-09-24 13:15:51 +0800

    feat: add design tokens and update favicons with build assets

--- scratch_inspect_dark.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const css = fs.readFileSync('ref_styles.css', 'utf8');
-
-const regex = /\[data-theme=["']?dark["']?\]\s*\{([^}]+)\}/g;
-let match;
-while ((match = regex.exec(css)) !== null) {
-  console.log('--- DARK THEME BLOCK ---');
-  console.log(match[1]);
-}
